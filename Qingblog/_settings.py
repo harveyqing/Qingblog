1711fa2fe7586111d3621b180bb81cc1c06be27d
harvey <harveyqing@gmail.com>
2014-03-13 08:30:49 +0800

    Update _settings.py

--- Qingblog/_settings.py
+++ Qingblog/_settings.py
@@ -53,8 +53,8 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = "harveyqing@gmail.com"
-MAIL_PASSWORD = "Qingtian9919"
+MAIL_USERNAME = "YOUR MAIL ACCOUNT"
+MAIL_PASSWORD = "YOUR MAIL PWD"
 
 # administrator list
 ADMINS = ['harveyqing@gmail.com']
